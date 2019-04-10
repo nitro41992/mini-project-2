36ff43b8ca3b3cef0cdc7684d4b68bd165441fcf
kuchiman <kuchiman@njit.edu>
2019-04-10 00:27:54 -0400

    Feature: 7.  Create a controller, route, and view to show a contact form and add it to the main menu of your site.

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="card mt-3 pl-2 pr-2">
+            <div class="card-title">
+                <h1>Contact</h1>
+                <p class="lead">Please use this to contact the site owner.</p>
+            </div>
+            <div class="card-body">
+                <form>
+                    @csrf
+                    <div class="form-group">
+                        <label for="name">Name</label>
+                        <input name="name" type="text" class="form-control" id="name" placeholder="First and last name">
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email address</label>
+                        <input name="email" type="email" class="form-control" id="email" placeholder="name@example.com">
+                    </div>
+                    <div class="form-group">
+                        <label for="body">Message</label>
+                        <textarea name="body" class="form-control" id="body" rows="3"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary mb-2">Send</button>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,7 +60,9 @@
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
-
+                                    <a class="dropdown-item" href="{{ route('contact') }}">
+                                       Contact
+                                    </a>
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
