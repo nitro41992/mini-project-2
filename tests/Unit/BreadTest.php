427a45b8d5ce98286ed9135b79eaf9ff7fdd8438
kuchiman <kuchiman@njit.edu>
2019-04-13 10:44:57 -0400

    Feature: 6.  Create a unit test to update the name of a user in the database to Steve Smith.

--- tests/Unit/BreadTest.php
+++ tests/Unit/BreadTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\DB;
 use \App\User;
 
 class BreadTest extends TestCase
@@ -13,10 +14,19 @@ class BreadTest extends TestCase
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testCreateUserTest()
     {
         $user = factory(User::class)->create();
         //dd($user);
         $this->assertDatabaseHas('users', ['id' => $user->id]);
+    }
+    public function testUpdateUserNameTest()
+    {
+        $user = factory(User::class)->create();
+        DB::table('users')
+            ->where('id', $user->id)
+            ->update(['name' => 'Steve Smith']);
+        dd($user);
+        $this->assertDatabaseHas('users', ['id' => $user->id, 'name' => 'Steve Smith']);
     }
 }
